09149f4cb7577d06905d0dc273e422288f2cc86d
ngard1234 <neilgardner@verizon.net>
2023-01-07 11:19:30 -0500

    Completed With Context exercise

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
 
 
 
@@ -8,14 +10,15 @@ const App = () => {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
+  let searchInput = useRef('')
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
-  useEffect(() => {
-      if(search) {
+  const handleSearch = (e, term) => {
+    e.preventDefault()
           const fetchData = async () => {
-              document.title = `${search} music`
-              const response = await fetch(API_URL + search)
+              document.title = `${term} music`
+              const response = await fetch(API_URL + term)
               const resData = await response.json()
               if (resData.results.length > 0) {
                   return setData(resData.results)
@@ -25,20 +28,28 @@ const App = () => {
           }
           fetchData()
       }
-  }, [search])
+  
 
-  const handleSearch = (e, term) => {
-      e.preventDefault()
-      setSearch(term)
-  }
+  //const handleSearch = (e, term) => {
+  //    e.preventDefault()
+  //    setSearch(term)
+  //}
 
-  return (
-      <div>
-          <SearchBar handleSearch={handleSearch} />
-          {message}
-          <Gallery data={data} />
-      </div>
-  )
+  
+    return (
+        <div className="App">
+            <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+            }}>
+                <SearchBar />
+            </SearchContext.Provider>
+            {message}
+            <DataContext.Provider value={data} >
+                <Gallery />
+            </DataContext.Provider>
+        </div>
+    )
 }
 
 export default App
